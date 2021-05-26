45b284e71c199b1985395600f17dc4d1b1b38bbf
vrarora <vraroraa@protonmail.com>
2021-05-26 23:37:12 +0530

    Skills Page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyNavbar from "./components/my-navbar/mynavbar.component";
 import MyCarousal from "./components/my-carousal/my-carousal.component";
 import TitleMessage from "./components/title-message/title-message.component";
 import About from "./pages/about/about.component";
+import Skills from './pages/skills/skills.component'
 
 import './App.css';
 
@@ -25,6 +26,14 @@ const App = () => {
               </Fade>
             </Container>
         </Parallax>
+      </div>
+      {/* Skills Section */}
+      <div class="container d-flex h-100">
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+              <Skills />
+              </Fade>
+            </Container>
       </div>
  </div>
   );
---
+++ src/pages/skills/skills-data.js
@@ -0,0 +1,29 @@
+import Astronaut from "../../assets/img/skills/Dogeastro.svg";
+import Dancer from "../../assets/img/skills/Dogedance.svg";
+import Gold from "../../assets/img/skills/Dogegold.svg";
+import Muscle from "../../assets/img/skills/Dogestrong.svg";
+
+export const skills = {
+    advantages: [
+      {
+        imgAltText: "Doge in Space Suit",
+        imgSrc: Astronaut,
+        skillName: "Astronaut",
+      },
+      {
+        imgAltText: "Doge Dancing",
+        imgSrc: Dancer,
+        skillName: "Dancer",
+      },
+      {
+        imgAltText: "Golden DogeCoin",
+        imgSrc: Gold,
+        skillName: "Can Turn into Gold",
+      },
+      {
+        imgAltText: "Muscular Doge",
+        imgSrc: Muscle,
+        skillName: "Stronk Doge",
+      },
+    ],
+};
