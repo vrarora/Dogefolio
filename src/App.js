958e1e1f1f46982ced70ad748d6fad9b20b93429
Vaibhav Ratnam Arora <vraroraa@protonmail.com>
2021-05-26 17:46:13 +0530

    initial commit: completed MyNavar component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// components
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+import './App.css';
+
+const App = () => {
+  return (
+<div>
+  <MyNavbar />
+  
+  My Portfolio </div>
+  );
+}
+
+export default App;
