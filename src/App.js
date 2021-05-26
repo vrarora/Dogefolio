d62aaaaed8d622564e3951cdd18f57e29ce65db5
vrarora <vraroraa@protonmail.com>
2021-05-26 19:46:21 +0530

    Added my-carousal, title-message, and scroll-down component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 // components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from "./components/title-message/title-message.component";
+
 import './App.css';
 
 const App = () => {
   return (
 <div>
   <MyNavbar />
-  
-  My Portfolio </div>
+  <MyCarousal />
+  <TitleMessage />
+ </div>
   );
 }
 
---
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import Slide1 from '../../assets/img/carousal/Doge1.jpg'
+import Slide2 from '../../assets/img/carousal/Doge6.jpg'
+import Slide3 from '../../assets/img/carousal/Doge9.jpg'
+import ScrollDown from '../scroll-down/scroll-down.component'
+
+
+import "./my-carousal.style.css";
+const MyCarousal = () => {
+    return (
+        <div id="home">
+           <Carousel controls={false} indicators interval={2500} pause={false}>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 custom-img"
+      src={Slide1}
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 custom-img"
+      src={Slide2}
+      alt="Second slide"
+    />
+
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 custom-img"
+      src={Slide3}
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel> 
+<ScrollDown /> 
+        </div>
+    )
+}
+
+export default MyCarousal;
